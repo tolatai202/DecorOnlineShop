27f0ade35928f2941935ce4be9592dc0e7f56608
tolatai202 <viettai.uneti@gmail.com>
2024-05-08 23:24:29 +0700

    init project

---
+++ DecorShop/DecorShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DecorShop.MvcApplication" Language="C#" %>
---
+++ DecorShop/DecorShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DecorShop.Startup))]
+namespace DecorShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
